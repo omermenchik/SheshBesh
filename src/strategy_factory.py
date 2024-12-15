27314e7ee74374fdc13e40e384f70a96b77aab9b
omermenchik <130279478+omermenchik@users.noreply.github.com>
2024-12-15 17:36:48 +0200

    Update strategy_factory.py

--- src/strategy_factory.py
+++ src/strategy_factory.py
@@ -1,15 +1,18 @@
-from src.compare_all_moves_strategy import CompareAllMovesSimple
+from src.compare_all_moves_strategy import CompareAllMovesSimple,CompareAllMovesMinMax
 from src.strategies import MoveFurthestBackStrategy, HumanStrategy, MoveRandomPiece
 
 
 class StrategyFactory:
     @staticmethod
-    def create_by_name(strategy_name):
+    def create_by_name(strategy_name, time_limit=5):
         for strategy in StrategyFactory.get_all():
             if strategy.__name__ == strategy_name:
+                # Pass time_limit and depth only to relevant strategies
+                if strategy in [CompareAllMovesSimple, CompareAllMovesMinMax]:
+                    return strategy(time_limit=time_limit)
                 return strategy()
 
-        raise Exception("Cannot find strategy %s" % strategy_name)
+        raise Exception(f"Cannot find strategy {strategy_name}")
 
     @staticmethod
     def get_all():
@@ -18,5 +21,6 @@ class StrategyFactory:
             MoveFurthestBackStrategy,
             CompareAllMovesSimple,
             HumanStrategy,
+            CompareAllMovesMinMax,
         ]
         return strategies
